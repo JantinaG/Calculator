6d3cff0678cd997342d68063b9981b5f03f0c48e
Jantina Eefting <65661797+JantinaG@users.noreply.github.com>
2021-10-10 22:47:14 +0100

    JS: calculation and displaying

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 let display = document.querySelector(".display");
-let allClearButton = document.querySelector(".all-clear");
-let backspaceButton = document.querySelector(".backspace");
-let operationButton = document.querySelectorAll(".operation");
-let numberButton = document.querySelectorAll(".number");
-let equalButton = document.querySelector(".equal");
+let historyDisplay = document.querySelector(".history");
 
-function updateDisplay(value) {}
+// display above (works)
+function updateDisplay(previousNumber) {
+  let history = historyDisplay.innerHTML;
+  if (history === "") {
+    historyDisplay.innerHTML = `${previousNumber}`;
+  } else {
+    historyDisplay.innerHTML = `${history}${previousNumber}`;
+  }
+}
 
+// defining function for on click (works)
 function insertNum(value) {
   let previousDisplay = display.innerHTML;
-  display.innerHTML = `${previousDisplay}${value}`;
+  if (isNaN(value)) {
+    let previousNumber = `${previousDisplay}${value}`;
+    updateDisplay(previousNumber);
+    console.log(previousNumber);
+    display.innerHTML = "";
+  } else {
+    display.innerHTML = `${previousDisplay}${value}`;
+  }
+}
+
+// calculation (works) *Need to fix: if 1+1+ = :error in console.log
+function equal() {
+  let previous = historyDisplay.innerHTML;
+  let current = display.innerHTML;
+  historyDisplay.innerHTML = "";
+  display.innerHTML = eval(`${previous}${current}`);
 }
 
 // allclear (works)
 function allClear() {
   display.innerHTML = "";
+  historyDisplay.innerHTML = "";
 }
 
-// backspac (works)
+// backspace (works only on .display)
 function backspace() {
   let operation = display.innerHTML;
   display.innerHTML = `${operation.slice(0, -1)}`;
